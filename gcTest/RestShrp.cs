a8548677dd711bb8e56bb01f470f2f5bb7e1accb
vadkostuk <vadkostuk@gmail.com>
2021-01-14 14:00:36 +0300

    added resthurp tests

---
+++ gcTest/RestShrp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+
+namespace gcTest
+{
+    class RestShrp
+    {
+        [Test]
+        public void restGetLanguageTest()
+        {
+            RestClient client = new RestClient("https://greencity.azurewebsites.net/");
+            RestRequest request = new RestRequest("/language", Method.GET);
+            IRestResponse restResponse = client.Execute(request);
+            string responseLanguage = restResponse.Content;
+            Assert.IsTrue(responseLanguage.Contains("[\"en\",\"ru\",\"ua\"]"));
+        }
+
+
+        [Test]
+        public void restSignInTest()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("email", "xdknxusqvjeovowpfk@awdrt.com");
+            jObject.Add("password", "Temp#001");
+            RestClient client = new RestClient("https://greencity.azurewebsites.net/");
+            RestRequest request = new RestRequest("/ownSecurity/signIn", Method.POST);
+            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+            IRestResponse restResponse = client.Execute(request);
+            Console.WriteLine(restResponse.Content);
+            string content = restResponse.Content;
+            Assert.IsTrue(content.Contains("accessToken"));
+        }
+    }
+}
